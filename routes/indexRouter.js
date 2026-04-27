0514f7ce72c1ab3f4300b03fa23dc14b87f7fb9d
GabrielBRQ <gabrielmedvalle@gmail.com>
2026-04-27 10:09:48 -0300

    add message details

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -20,6 +20,21 @@ indexRouter.get("/", (req, res) => {
   res.render("index", { messages: messages });
 });
 
+indexRouter.get("/messageDetails/:id", (req, res) => {
+  const messageId = req.params.id;
+  
+  const selectedMessage = messages[messageId];
+
+  if (!selectedMessage) {
+    return res.status(404).send("Mensagem não encontrada!");
+  }
+
+  res.render("messageDetails", { 
+    user: selectedMessage.user, 
+    message: selectedMessage.text 
+  });
+});
+
 indexRouter.get("/new", (req, res) => {
   res.render("form");
 });
